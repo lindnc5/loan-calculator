8d37a8f0abd91d246b9f9d57e48197f0c7b5e560
Lindsey Chafin <lindnc5@vt.edu>
2020-06-30 22:38:54 -0400

    inital styling with Material

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MainFormComponent from './inputForm';
+import React from "react";
 
-
+import "./App.css";
+import MainFormComponent from "./inputForm";
+import Container from "@material-ui/core/Container";
+import { AppBar, Typography, Card, CardContent } from "@material-ui/core";
 
 function App() {
-
-
-  
-
   return (
     <div className="App">
-     <MainFormComponent/>
+      <AppBar style={{flexGrow: 1}} position="static">
+        <Typography style={{margin: 5}} variant="h3">Payoff</Typography>
+      </AppBar>
+      <Container maxWidth="md" className="Container">
+        <Card style={{ height: "100vh" }}>
+          <CardContent>
+          <MainFormComponent />
+          </CardContent>
+        </Card>
+      </Container>
     </div>
   );
 }
-
 
 export default App;
--- src/inputForm.js
+++ src/inputForm.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import { updateInitialAmount, updateInterestRate } from "./actions";
+import { TextField, makeStyles, Grid, Button } from "@material-ui/core";
+
+let useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 class MainFormComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = { va1: 0, val2: 0 };
   }
+
+  
 
   handleSubmit = (event) => {
     alert("Your total is :" + this.state.val1 * this.state.val2);
@@ -23,23 +35,32 @@ class MainFormComponent extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <form>
-          <label>
-            Num1:
-            <input onChange={this.changeit1} type="number" name="num1" />
-          </label>
-          <label>
-            Num2:
-            <input onChange={this.changeit2} type="number" name="num2" />
-          </label>
-          <input
+         
+          <Grid >
+          <div>
+          <TextField
+            label='Initial Amount'
+             onChange={this.changeit1} type="number" name="num1" variant="outlined">
+            </TextField>
+            </div>
+            <div>
+          <TextField
+            label='Interest Rate'
+             onChange={this.changeit2} type="number" name="num2" variant="outlined"></TextField>
+          </div>
+          <div>
+          <TextField
             value={this.props.form.initialAmount}
             type="number"
-            name="num3"
-          />
+            name="num3" variant="outlined"></TextField>
+            </div>
+         
 
-          <button onClick={this.handleSubmit}>Submit</button>
+          <Button onClick={this.handleSubmit}>Submit</Button>
+          </Grid>
+          
         </form>
       </div>
     );
